78232ceefa7b3605ccf31469709aeb54bce94f24
sofiya111516 <sofiyashaikh56024@gmail.com>
2026-01-18 00:55:18 +0530

    Update config.py

--- pm_internship_backend/config.py
+++ pm_internship_backend/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""   # yaha apna MySQL password daalna hai
+    MYSQL_PASSWORD = ""   
     MYSQL_DATABASE = "pm_internship"
