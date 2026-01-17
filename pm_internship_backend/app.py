85e931da9b351dff6e5dd31d2f46e5f230ab7ddc
sofiya111516 <sofiyashaikh56024@gmail.com>
2026-01-18 00:54:51 +0530

    Update app.py

--- pm_internship_backend/app.py
+++ pm_internship_backend/app.py
@@ -5,24 +5,18 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 CORS(app)
 
-
-# -------- MySQL Config --------
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_PORT'] = 3307
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pm_internship'
 
-# -------- Upload Config --------
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # 5MB
 
-# Initialize mysql
 mysql = MySQL(app)
 app.config["MYSQL"] = mysql
 
-
-# -------- Register Routes --------
 from routes.auth_routes import auth
 from routes.profile_routes import profile
 from routes.upload_routes import upload
@@ -35,10 +29,6 @@ app.register_blueprint(upload, url_prefix="/upload")
 app.register_blueprint(internship, url_prefix="/internships")
 app.register_blueprint(applications, url_prefix="/applications")
 
-
-
-# -------- Static Routes for files --------
-
 @app.route('/uploads/photos/<filename>')
 def photos(filename):
     return send_from_directory("uploads/photos", filename)
@@ -47,13 +37,10 @@ def photos(filename):
 def resumes(filename):
     return send_from_directory("uploads/resumes", filename)
 
-
-# -------- ROOT --------
 @app.route("/")
 def home():
     return {"message": "Backend Running"}
 
 
-# -------- RUN --------
 if __name__ == "__main__":
     app.run(debug=True)
